616d1bb2ed94be2fbaba824418bc0398ae9f4dcc
hrishi <varun.hrishi.18@gmail.com>
2024-01-03 18:44:45 +0530

    Added credential's validation to the login api

--- main.go
+++ main.go
@@ -2,37 +2,74 @@ package main
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
 
 type User struct {
-	Username string `json:"username"`
-	PassWord string `json:"password"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Name        string `json:"name"`
+	Team        string `json:"team"`
+	Designation string `json:"designation"`
+	Approver    string `json:"approver"`
 }
 
+var payload []User
+var content, _ = os.ReadFile("./users.json")
+
+var usersMap = map[string]User{}
+
 func main() {
+	err := json.Unmarshal(content, &payload)
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < len(payload); i++ {
+		user := payload[i]
+		usersMap[user.Username] = user
+	}
+
 	routes := mux.NewRouter()
 	routes.HandleFunc("/login", handleLogin).Methods("GET")
 
 	http.ListenAndServe(":8080", routes)
 }
 
+// handle login
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-	var user User
 	fmt.Println("Service is responding")
+
+	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		panic(errors.New(
-			"Could not parse/decode the json request. Make sure that the json is properly formatted and has all the required fields."))
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Bad Request payload!!!"))
+		return
 	}
 
 	//Authenticate the user credentials with the database
-	//validateCred(user);
+	user = validateCred(usersMap, user)
+	if (User{} == user) {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Invalid Credentials!!"))
+		return
+	}
 
 	response, _ := json.Marshal(user)
 	w.Write(response)
+}
+
+// function to validate the user
+func validateCred(userList map[string]User, userToAuthorize User) User {
+	user, found := userList[userToAuthorize.Username]
+	if found && user.Password == userToAuthorize.Password {
+		return user
+	}
+
+	return User{}
 }
