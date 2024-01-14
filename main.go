930469f7fd7d24069a75276315f8ee40efad20de
hrishi <varun.hrishi.18@gmail.com>
2024-01-15 02:44:16 +0530

    Wrote prototype of aplying for leaves

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
@@ -18,24 +19,50 @@ type User struct {
 	Approver    string `json:"approver"`
 }
 
-var payload []User
-var content, _ = os.ReadFile("./users.json")
+type LeaveSpan struct {
+	Start string `json:"startTime"`
+	End   string `json:"endTime"`
+}
+
+type Leave struct {
+	Username string      `json:"username"`
+	Leaves   []LeaveSpan `json:"leaves"`
+}
+
+var userDatabase []User
+var userDatabaseContent, _ = os.ReadFile("./usersDatabase.json")
+
+var leaveDatabase []Leave
+var leaveDatabaseContent, _ = os.ReadFile("./leaveDatabase.json")
 
 var usersMap = map[string]User{}
+var leaveMap = map[string][]LeaveSpan{}
 
 func main() {
-	err := json.Unmarshal(content, &payload)
+
+	err := json.Unmarshal(userDatabaseContent, &userDatabase)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	for i := 0; i < len(payload); i++ {
-		user := payload[i]
+	for i := 0; i < len(userDatabase); i++ {
+		user := userDatabase[i]
 		usersMap[user.Username] = user
+	}
+
+	err = json.Unmarshal(leaveDatabaseContent, &leaveDatabase)
+	if err != nil {
+		log.Fatal("err")
+	}
+
+	for i := 0; i < len(leaveDatabase); i++ {
+		user := leaveDatabase[i].Username
+		leaveMap[user] = leaveDatabase[i].Leaves
 	}
 
 	routes := mux.NewRouter()
 	routes.HandleFunc("/login", handleLogin).Methods("GET")
+	routes.HandleFunc("/apply", handleApply).Methods("PUT")
 
 	http.ListenAndServe(":8080", routes)
 }
@@ -47,14 +74,16 @@ func handleLogin(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
+		log.Fatal(err)
 		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("Bad Request payload!!!"))
+		w.Write([]byte("Bad Request userDatabase!!!"))
 		return
 	}
 
 	//Authenticate the user credentials with the database
 	user = validateCred(usersMap, user)
 	if (User{} == user) {
+		log.Fatal(err)
 		w.WriteHeader(http.StatusUnauthorized)
 		w.Write([]byte("Invalid Credentials!!"))
 		return
@@ -72,4 +101,34 @@ func validateCred(userList map[string]User, userToAuthorize User) User {
 	}
 
 	return User{}
+}
+
+// handle leave application
+func handleApply(w http.ResponseWriter, r *http.Request) {
+	var leaveApplication Leave
+	err := json.NewDecoder(r.Body).Decode(&leaveApplication)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	leaveToAppend := LeaveSpan{
+		Start: leaveApplication.Leaves[0].Start,
+		End:   leaveApplication.Leaves[0].End,
+	}
+
+	for i := 0; i < len(leaveDatabase); i++ {
+		if leaveDatabase[i].Username == leaveApplication.Username {
+			leaveDatabase[i].Leaves = append(leaveDatabase[i].Leaves, leaveToAppend)
+			break
+		}
+	}
+	leaveDatabaseContent, _ := json.Marshal(leaveDatabase)
+	if err := os.WriteFile("leaveDatabase.json", leaveDatabaseContent, 0666); err != nil {
+		log.Fatal(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	response, _ := json.Marshal(leaveDatabaseContent)
+	w.Write(response)
 }
