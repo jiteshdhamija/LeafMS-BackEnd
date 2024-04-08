704c5f3238058ec14ac4ed9cd67c0582f0db1c61
hrishivarun <varun.hrishi.18@gmail.com>
2024-04-09 01:46:35 +0530

    auth working properly, but broke /apply APi endpoint. :)

--- main.go
+++ main.go
@@ -12,10 +12,11 @@ func main() {
 
 	routes := mux.NewRouter()
 	routes.HandleFunc("/login", handleLogin).Methods("GET")
-	routes.Use(handleAuth)
-	routes.HandleFunc("/apply", handleApply).Methods("PUT")
-	routes.HandleFunc("/leaves", handleViewLeaves).Methods("GET")
-	routes.HandleFunc("/aprrove", handleLeaveApproval).Methods("PUT")
+	authRoute := routes.NewRoute().Subrouter()
+	authRoute.Use(handleAuth)
+	authRoute.HandleFunc("/apply", handleApply).Methods("PUT")
+	authRoute.HandleFunc("/leaves", handleViewLeaves).Methods("GET")
+	authRoute.HandleFunc("/aprrove", handleLeaveApproval).Methods("PUT")
 
 	http.ListenAndServe(":8080", routes)
 }
